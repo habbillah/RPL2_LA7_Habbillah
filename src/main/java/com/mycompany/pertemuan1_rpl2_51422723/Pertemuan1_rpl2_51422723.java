80cce73cf74134fe4cb2f7934b90d78435100e44
unknown <habbillahbashuda@gmail.com>
2025-12-04 15:09:40 +0700

    first commit

---
+++ src/main/java/com/mycompany/pertemuan1_rpl2_51422723/Pertemuan1_rpl2_51422723.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pertemuan1_rpl2_51422723;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pertemuan1_rpl2_51422723 {
+
+    public static void main(String[] args) {
+        int a =5;
+        int b = 10;
+        int hasil = a+b;
+        
+        System.out.println("Hasil akhirnya adalah : " + hasil);
+        
+       Scanner input = new Scanner(System.in);
+       
+       System.out.print("Masukkan nama anda");
+       
+       String nama = input.nextLine();
+       
+       System.out.println("Halo, " + nama);
+       
+       for (int i = 0; i <=10 i++) {
+           System.out,println("Angka ke-" +i);
+       }
+       
+       int angka = 3;
+       
+       if(angka > 0) {
+           System.out.println("Angka positif");
+       }
+    }
+}
